577721413805d98b55c3e351674b74966337de66
connor <conclancy@hotmail.com>
2020-11-21 14:31:52 -0500

    Initial commit

---
+++ PayrollCoreApp/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace PayrollCoreApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Staff connor = new Staff("Connor",30f);
+            connor.HoursWorked = 40;
+            connor.CalculatePay();
+            Console.WriteLine(connor.ToString());
+        }
+    }
+}
---
+++ PayrollCoreApp/Staff.cs
@@ -0,0 +1,46 @@
+﻿using System;
+namespace PayrollCoreApp
+{
+    public class Staff
+    {
+        private float hourlyRate;
+        private int hWorked;
+
+        public float TotalPay { get; protected set; }
+        public float BasicPay { get; private set; }
+        public string NameOfStaff { get; private set; }
+        public int HoursWorked
+        {
+            get
+            {
+                return hWorked;
+            }
+            set
+            {
+                if (value > 0)
+                    hWorked = value;
+                else
+                    hWorked = 0;
+            }
+
+        }
+
+        public Staff(string name, float rate)
+        {
+            NameOfStaff = name;
+            hourlyRate = rate;
+        }
+
+        public virtual void CalculatePay()
+        {
+            Console.WriteLine("Calculating Pay...");
+            BasicPay = hWorked * hourlyRate;
+            TotalPay = BasicPay;
+        }
+
+        public override string ToString()
+        {
+            return $"\nName: {NameOfStaff} \nHourly Rate: {hourlyRate:c} \nHours Worked: {HoursWorked} \nBase Pay: {BasicPay:c} \nTotal Pay: {TotalPay:c}";
+        }
+    }
+}
