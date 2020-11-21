3aff62b178999e1d9fd56d2ed0a1f2617487b127
connor <conclancy@hotmail.com>
2020-11-21 15:41:47 -0500

    Changed HoursWorked from private to protected

--- PayrollCoreApp/Staff.cs
+++ PayrollCoreApp/Staff.cs
@@ -3,9 +3,11 @@ namespace PayrollCoreApp
 {
     public class Staff
     {
-        private float hourlyRate;
+        // Create feilds
+        protected float hourlyRate;
         private int hWorked;
 
+        // Create properties
         public float TotalPay { get; protected set; }
         public float BasicPay { get; private set; }
         public string NameOfStaff { get; private set; }
@@ -25,19 +27,22 @@ namespace PayrollCoreApp
 
         }
 
+        // Constructor for Staff object
         public Staff(string name, float rate)
         {
             NameOfStaff = name;
             hourlyRate = rate;
         }
 
+        // Method for calculating basic and total pay
         public virtual void CalculatePay()
         {
-            Console.WriteLine("Calculating Pay...");
+            Console.WriteLine("\nCalculating Pay...");
             BasicPay = hWorked * hourlyRate;
             TotalPay = BasicPay;
         }
 
+        // Method for overriding ToString() method
         public override string ToString()
         {
             return $"\nName: {NameOfStaff} \nHourly Rate: {hourlyRate:c} \nHours Worked: {HoursWorked} \nBase Pay: {BasicPay:c} \nTotal Pay: {TotalPay:c}";
